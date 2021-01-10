c6c8d66c712807cc840df8bfbf12fcb57f6c7f77
dantmnf <dantmnf@users.noreply.github.com>
2021-01-10 16:31:33 +0800

    update

--- app/src/main/java/xyz/cirno/wcgtest/EntryActivity.java
+++ app/src/main/java/xyz/cirno/wcgtest/EntryActivity.java
@@ -2,8 +2,12 @@ package xyz.cirno.wcgtest;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class EntryActivity extends Activity {
     @Override
@@ -13,5 +17,9 @@ public class EntryActivity extends Activity {
         findViewById(R.id.launch_ui_test_button).setOnClickListener(v -> startActivity(new Intent(this, MainActivity.class)));
         findViewById(R.id.launch_gl_test_button).setOnClickListener(v -> startActivity(new Intent(this, GLActivity.class)));
         findViewById(R.id.launch_browser_test_button).setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://dantmnf.github.io/wcgtest/"))));
+        Display d = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+        ((TextView)findViewById(R.id.textView6)).setText("Display.isWideColorGamut() => " + d.isWideColorGamut());
+        ((TextView)findViewById(R.id.textView24)).setText("Configuration.isScreenWideColorGamut() => " + Resources.getSystem().getConfiguration().isScreenWideColorGamut());
+
     }
 }
--- app/src/main/java/xyz/cirno/wcgtest/MainActivity.java
+++ app/src/main/java/xyz/cirno/wcgtest/MainActivity.java
@@ -1,7 +1,15 @@
 package xyz.cirno.wcgtest;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
@@ -10,6 +18,13 @@ public class MainActivity extends Activity {
         getActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle(R.string.android_ui_test_title);
         this.setContentView(R.layout.activity_main);
+        Window w = getWindow();
+        findViewById(R.id.switch1).setOnClickListener((view)->{
+            Switch s = (Switch)view;
+            w.setColorMode(s.isChecked() ? ActivityInfo.COLOR_MODE_WIDE_COLOR_GAMUT : ActivityInfo.COLOR_MODE_DEFAULT);
+        });
+
+
     }
 
     @Override
